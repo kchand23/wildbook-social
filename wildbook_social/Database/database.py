9d8e3ccbf48c4bd216229b305ae8c5214da9e321
mramir71 <51290254+mramir71@users.noreply.github.com>
2019-08-15 08:39:16 -0500

    Added manual location insertion for youtube only

--- wildbook_social/Database/database.py
+++ wildbook_social/Database/database.py
@@ -3,6 +3,7 @@ from IPython.display import YouTubeVideo, Image, display
 from datetime import timedelta
 import dateutil.parser
 import matplotlib.pyplot as plt
+import csv
 
 class Database:
     def __init__(self, key, database):
@@ -24,7 +25,7 @@ class Database:
     def doStatistics(self, collection, amount):
         i = 1
         while(amount > 0):
-            item = self.db[collection].find_one({"$or":[{"relevant":None}, {"wild":None}]})
+            item = self.db[collection].find_one({"$or":[{"relevant":None}, {"wild":None}]}) #{'recordingDetails.location': None}]})
             if not item:
                 break
             
@@ -41,15 +42,39 @@ class Database:
             if rel == True:
                 print("Wild (y/n):", end =" ")
                 wild = True if input() == "y" else False
+                if wild == True:
+                    
+                    #add in location option, only for youtube videos
+                    if self.dbName == 'youtube':
+                        if item['recordingDetails']['location'] == None: 
+                            print("Is there a location? (y/n):", end =" ")
+                            if input() == "y": 
+                                print("Enter location (city,country):", end = " ")
+                                loc = input()
+                            else:
+                                loc = 0   
             if rel == False:
                 wild = 0 #bc cannot determine a video to be wild if it is not relevant 
-            
-            self._updateItem(collection, item['_id'], {"relevant": rel, "wild": wild})
-            
+                if self.dbName == 'youtube':
+                    loc = 0
+                
+              
+                
+            #update with new values
+            if self.dbName == 'youtube':
+                self._updateItem(collection, item['_id'], {"relevant": rel, "wild": wild,"newLocation": loc })
+                print("Response saved! Location : {}.\n".format(loc))
+            else:
+                self._updateItem(collection, item['_id'], {"relevant": rel, "wild": wild})
+                
             print("Response saved! {} and {}.\n".format("Relevant" if rel else "Not relevant", "Wild" if wild else "Not wild"))
+           # print("Response saved! Location : {}.\n".format(loc))
+            
+            
             amount -= 1
             i += 1
         print('No more items to proceed.')
+    
             
     def _updateItem(self, collection, id, payload):
         try:
@@ -82,6 +107,7 @@ class Database:
 
         # Convert the times from datetime format to YYYY-MM-DD format
         dates = [dateutil.parser.parse(x).date() for x in timePosts]
+        #sort the converted rates in a list with most recent at beginning and least recent towards end
         dates.sort()
         
         # Find the difference in days between posting dates of successive posts
@@ -99,14 +125,32 @@ class Database:
         # diffInDays = (largestDifference - smallestDifference).days
 
         # Plotting the histogram
-        #bins = [x*10 for x in range(11)]
         plt.figure(figsize=(15,5))
-        #changed bins = 10, allowing for 10 dynamic categories to be created based on data. Functionality of matplotlib. 
-        plt.hist(timeDiffs, bins = 10, histtype = 'bar', rwidth = 0.8)  
+        plt.hist(timeDiffs, bins = 10, histtype = 'bar', rwidth = 0.8)
         plt.xlabel('Days between succesive posts')
         plt.ylabel('Number of posts')
         plt.title('Histogram for Time Between Succesive Wild Posts')
         plt.show()
+        
+    #customized to youtube only so far
+    def heatmap(self,collection, csvName):
+        self.csvName = csvName +'.csv'
+        docs_w_loc = self.db[collection].find({'$and': [{'wild': True}, {'newLocation':{'$ne':0}}]})
+        loc_list = []
+        for doc in docs_w_loc:
+            dic = {
+                'videoID' : doc['videoID'],
+                'newLocation':doc['newLocation']
+            }
+            loc_list.append(dic)
+        
+        fields = ['videoID', 'newLocation'] 
+        with open(self.csvName, 'w') as locations_csv:
+            csvName = csv.DictWriter(locations_csv, fieldnames = fields)
+            csvName.writeheader()
+            for item in loc_list:
+                csvName.writerow(item)
+        print('done! Check in your jupyter files for a .csv file with the name you entered')
              
     def clearCollection(self, collection, msg=''):
         if (msg == 'yes'):
